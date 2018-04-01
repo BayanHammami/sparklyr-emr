3cbef2247c321214e3e3a5174df05f69a56ba97e
BayanHammami <bayan.hammami@gmail.com>
2018-04-01 13:06:11 +0000

    added config

--- sparklyr-emr.r
+++ sparklyr-emr.r
@@ -1,74 +1,14 @@
 library(sparklyr)
 library(dplyr)
-
+source('config.r')
 # create the Spark context, for EMR 5.2 use version = "2.0.0", for EMR 4.7/4.8 use version = "1.6.2", and for EMR 4.6 use "1.6.1"
 sc <- spark_connect(master = "yarn-client", version = "2.1.0")
 
-# load dplyr
+#Check status of emr cluster
+#aws emr describe-cluster --cluster-id j-1BD6D2VCR6ZXB --region ap-southeast-2
 
-# copy some builtin sample data to the Spark cluster
-iris_tbl <- copy_to(sc, iris)
-flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
-batting_tbl <- copy_to(sc, Lahman::Batting, "batting")
-src_tbls(sc)
+path <- "s3://domaindatateam/Data Science and Data Quality/CrossClusterMovement/antony_custom_dimensions"
 
-# filter by departure delay and print the first few records
-flights_tbl %>% filter(dep_delay == 2)
-
-batting_tbl %>% summarise(count = n())
-
-# plot data on flight delays
-delay <- flights_tbl %>% 
-  group_by(tailnum) %>%
-  summarise(count = n(), dist = mean(distance), delay = mean(arr_delay)) %>%
-  filter(count > 20, dist < 2000, !is.na(delay)) %>%
-  collect
-
-library(ggplot2)
-ggplot(delay, aes(dist, delay)) +
-  geom_point(aes(size = count), alpha = 1/2) +
-  geom_smooth() +
-  scale_size_area(max_size = 2)
-
-# window functions  
-batting_tbl %>%
-  select(playerID, yearID, teamID, G, AB:H) %>%
-  arrange(playerID, yearID, teamID) %>%
-  group_by(playerID) %>%
-  filter(min_rank(desc(H)) <= 2 & H > 0)
-
-# use SQL  
-library(DBI)
-iris_preview <- dbGetQuery(sc, "SELECT * FROM iris LIMIT 10")
-iris_preview
-
-# Machine learning example using Spark MLlib
-
-# copy the mtcars data into Spark
-mtcars_tbl <- copy_to(sc, mtcars)
-
-# transform our data set, and then partition into 'training', 'test'
-partitions <- mtcars_tbl %>%
-  filter(hp >= 100) %>%
-  mutate(cyl8 = cyl == 8) %>%
-  sdf_partition(training = 0.5, test = 0.5, seed = 1099)
-
-# fit a linear regression model to the training dataset
-fit <- partitions$training %>%
-  ml_linear_regression(response = "mpg", features = c("wt", "cyl"))
-fit
-
-summary(fit)
-
-# get the 10th row in test data
-car <- tbl_df(partitions$test) %>% slice(10)
-# predict the mpg 
-predicted_mpg <- car$cyl * fit$coefficients["cyl"] + car$wt * fit$coefficients["wt"] + fit$coefficients["(Intercept)"]
-
-# print the original and the predicted
-sprintf("original mpg = %s, predicted mpg = %s", car$mpg, predicted_mpg)
-
-
-
-
+Sys.setenv(AWS_ACCESS_KEY_ID=aws_access_key_id)
+Sys.setenv(AWS_SECRET_ACCESS_KEY=aws_secret_access_key)
 
